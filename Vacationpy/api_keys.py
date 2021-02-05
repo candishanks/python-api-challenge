2f4f30cb0562a1d88748ce5dade6039ed4928d14
Candi Shanks <candishanks@gmail.com>
2021-02-05 14:44:58 -0500

    updating changes

---
+++ Vacationpy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "AIzaSyARjnB0VEZin7jCSQ4BIrBMOG3gjAtKAWE"
---
+++ Weatherpy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "9316fd63ba23efd207424e1fccb3e8ec"
+
+# Google API Key
+g_key = "5117cc6f96f1d3e807391804cf3ca3b8"
